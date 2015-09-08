0b3c9bdd4592b18990128773f702fb2908407ee7
Joseph Pena <laaguila92@yahoo.com>
2015-09-08 16:28:56 -0400

    Python first practice exercises

---
+++ HelloWorld/first/Temperature.py
@@ -0,0 +1,11 @@
+'''
+Created on Sep 8, 2015
+
+@author: Admin
+'''
+def main() :
+    tempC = float (input("Input the temperature in Celsius: "))
+    tempF = (tempC * 1.8 + 32)
+    print ("Temperature in Celcius : ", tempC, ", in Fahrenheit is : ", tempF)
+    
+main()
---
+++ HelloWorld/first/nFactorial.py
@@ -0,0 +1,18 @@
+'''
+Created on Sep 8, 2015
+
+@author: Admin
+'''
+def main():
+    f=1
+    n=0
+    for n in range(19):
+        n=n+1
+        f=n*f
+        print(n, end = " ")
+        print(f)
+        print (n !=f)
+       
+        
+      
+main()
