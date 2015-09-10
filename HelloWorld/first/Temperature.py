08acd9ffa0f7ad8856ebb1cc1ec5f0772531e6f7
Joseph Pena <laaguila92@yahoo.com>
2015-09-10 09:53:29 -0400

    Python first day practice exercises

--- HelloWorld/first/Temperature.py
+++ HelloWorld/first/Temperature.py
@@ -4,8 +4,8 @@ Created on Sep 8, 2015
 @author: Admin
 '''
 def main() :
-    tempC = float (input("Input the temperature in Celsius: "))
-    tempF = (tempC * 1.8 + 32)
-    print ("Temperature in Celcius : ", tempC, ", in Fahrenheit is : ", tempF)
+    tempC = float (input("Input the temperature in Celsius: "))#the user inputs the temperature in celsius
+    tempF = (tempC * 1.8 + 32)#formula for converting celsius into fahrenheit
+    print ("Temperature in Celcius : ", tempC, ", in Fahrenheit is : ", tempF)#prints the temperature in celsius and what it is in fahrenheit
     
 main()
--- HelloWorld/first/nFactorial.py
+++ HelloWorld/first/nFactorial.py
@@ -5,10 +5,10 @@ Created on Sep 8, 2015
 '''
 import math
 def main():
-    n=0
-    for n in range(20):
-        f= math.factorial (n)
-        print (n, "!=", f)
+    n=0#set n to 0
+    for n in range(20):# setting the range for the for loop
+        f= math.factorial (n)#factorial function for the incremented variable n=0
+        print (n, "!=", f)#prints the factorial of n ranged to 20
         
       
 main()
