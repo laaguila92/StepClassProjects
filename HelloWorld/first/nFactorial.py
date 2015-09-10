ede6ba0ec1b2ef866c53054d01f60faab68fa95c
Joseph Pena <laaguila92@yahoo.com>
2015-09-10 08:49:16 -0400

    Python first practice exercises

--- HelloWorld/first/nFactorial.py
+++ HelloWorld/first/nFactorial.py
@@ -3,16 +3,12 @@ Created on Sep 8, 2015
 
 @author: Admin
 '''
+import math
 def main():
-    f=1
     n=0
-    for n in range(19):
-        n=n+1
-        f=n*f
-        print(n, end = " ")
-        print(f)
-        print (n !=f)
-       
+    for n in range(20):
+        f= math.factorial (n)
+        print (n, "!=", f)
         
       
 main()
