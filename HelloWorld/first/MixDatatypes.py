a536c3cedccf325706bd78c96ef0cc463a257068
Joseph Pena <laaguila92@yahoo.com>
2015-09-11 08:49:20 -0400

    Mixed datatype list with user input

---
+++ HelloWorld/first/MixDatatypes.py
@@ -0,0 +1,26 @@
+'''
+Created on Sep 10, 2015
+
+@author: Admin
+'''
+def mixdata():
+    a= str(input("What is your favorite season?"))
+    b= str(input("What is your favorite month?"))
+    c= str(input("What is your favorite word?"))
+    d= int(input("What is your favorite number?"))
+    e= int(input("What day of the month does your birthday fall on?"))
+    f= int(input("What is 2+2?"))
+    userlist= [a, b, c, d, e, f]
+    print(userlist)
+    print(d+e+f)
+    stringlist= [a, b, c]
+    print(stringlist)
+   
+   
+    
+mixdata()
+   
+   
+    
+    
+    
