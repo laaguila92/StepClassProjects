43af5a450f95dc840269fad0f492d65a1cd0da06
Joseph Pena <laaguila92@yahoo.com>
2015-09-11 09:13:27 -0400

    Mixed datatype list with user input

--- HelloWorld/first/MixDatatypes.py
+++ HelloWorld/first/MixDatatypes.py
@@ -4,17 +4,17 @@ Created on Sep 10, 2015
 @author: Admin
 '''
 def mixdata():
-    a= str(input("What is your favorite season?"))
-    b= str(input("What is your favorite month?"))
-    c= str(input("What is your favorite word?"))
-    d= int(input("What is your favorite number?"))
-    e= int(input("What day of the month does your birthday fall on?"))
-    f= int(input("What is 2+2?"))
-    userlist= [a, b, c, d, e, f]
-    print(userlist)
-    print(d+e+f)
-    stringlist= [a, b, c]
-    print(stringlist)
+    a= str(input("What is your favorite season?"))#asks the user for input in the form of a string and assigns it to the variable "a"
+    b= str(input("What is your favorite month?"))#asks the user for input in the form of a string and assigns it to the variable "b"
+    c= str(input("What is your favorite word?"))#asks the user for input in the form of a string and assigns it to the variable "c"
+    d= int(input("What is your favorite number?"))#asks the user for input in the form of an integer and assigns it to the variable "d"
+    e= int(input("What day of the month does your birthday fall on?"))#asks the user for input in the form of an integer and assigns it to the variable "e"
+    f= int(input("What is 2+2?"))#asks the user for input in the form of an integer and assigns it to the variable "f"
+    userlist= [a, b, c, d, e, f]#creates a list composed of the user's input, combining the ints and the strings in one list
+    print(userlist)# prints out the list "userlist"
+    print(d+e+f)#adds the user inputed integers together and prints it
+    stringlist= [a, b, c]#creates a  list of only the user inputed strings
+    print(stringlist)#prints the list "stringlist"
    
    
     
